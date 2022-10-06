f9d3dd870ff8b9bbb9963afe1bc9a67febad4774
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado@users.noreply.github.com>
2022-10-06 00:13:05 -0300

    Update CalculoSalario.java

--- CalculoSalario.java
+++ CalculoSalario.java
@@ -3,9 +3,9 @@ public class CalculoSalario {
     public static void main (String [] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Qual seu ganho por hora?:");
-        double ganho = Double.parseDouble(scanner.next());
+        double ganho = scanner.nextDouble();
         System.out.println("Quantas horas você trabalhou esse mês?:");
-        double horas = Double.parseDouble(scanner.next());
+        double horas = scanner.nextDouble();
         double salario = ganho*horas;
         System.out.println("Seu ganho total foi igual a: "+ salario+".");
     }
